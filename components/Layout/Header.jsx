c6476d97988319c2f841e03b49cd12feb9ef9073
nalendramuhammad <itsnalendraa@gmail.com>
2024-01-14 20:29:45 +0700

    responsive

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 
 export default function Header() {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const handleContactClick = () => {
     setModalOpen(true);
@@ -14,24 +15,35 @@ export default function Header() {
     setModalOpen(false);
   };
 
+  const handleMenuClick = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={`${style.navbar}`}>
       <div className={`${style.navbarLogo}`}>
         <p>N</p>
       </div>
-      <div className={`${style.navbarLeft}`}>
-        <Link href={"/"}>
-          <p href="">Home</p>
-        </Link>
-        <Link href={"/experience"}>
-          <p href="">Experience</p>
-        </Link>
-        <Link href={"/portfolio"}>
-          <p href="">Portfolio</p>
-        </Link>
+      <div className={`${style.navbarMenu}`}>
+        <div className={`${style.navbarLeft}`}>
+          <Link href={"/"}>
+            <p href="">Home</p>
+          </Link>
+          <Link href={"/experience"}>
+            <p href="">Experience</p>
+          </Link>
+          <Link href={"/portfolio"}>
+            <p href="">Portfolio</p>
+          </Link>
+        </div>
+        <div className={`${style.navbarRight}`}>
+          <button onClick={handleContactClick}>Contact</button>
+        </div>
       </div>
-      <div className={`${style.navbarRight}`}>
-        <button onClick={handleContactClick}>Contact</button>
+      <div className={`${style["hamburger-menu"]}`} onClick={handleMenuClick}>
+        <div className={`${style["bar"]}`}></div>
+        <div className={`${style["bar"]}`}></div>
+        <div className={`${style["bar"]}`}></div>
       </div>
       {isModalOpen && <Modal onClose={handleCloseModal} />}
     </div>
